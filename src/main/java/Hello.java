a8a925a3e8712d807d1985b15346c0a0a152b2b3
gstorohuk <galyna.storozhuk@gmil.com>
2021-01-26 15:26:31 -0500

    Updates Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning Git");
+
+        // watching more movie
+        // ready to go back to a leaving room
     }
 }
