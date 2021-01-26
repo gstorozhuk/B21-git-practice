040212b44f4cbd68cd6d8a7a7cebbc97ed601bc2
gstorohuk <galyna.storozhuk@gmil.com>
2021-01-26 15:38:16 -0500

    Updates Hello.java

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,9 +1,10 @@
 public class HelloTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello test");
+        System.out.println("Hello test 2");
 
         // test test test
+        // movie on the phone
 
     }
 }
